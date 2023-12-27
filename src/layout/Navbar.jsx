4205c8beb39621640496276def46a937ecd85884
Egor <65542192+Sillor@users.noreply.github.com>
2023-12-26 17:39:30 -0800

    Improve padding

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="flex items-center p-6">
+    <nav className="flex items-center px-1 md:p-6 p-6">
       <h1 className="nav--title font-extrabold text-2xl md:text-4xl hideoverlay mr-auto">
         <Link to="/">TapLegacy</Link>
       </h1>
       <Link to="achievements" className="hideoverlay">
-        <button className="nav--achievements text-lg md:text-2xl font-extrabold border-2 border-white-400 backdrop-filter backdrop-blur-sm bg-black bg-opacity-10 rounded-xl p-2 sm:p-4 mr-4 transition hover:scale-105">
+        <button className="nav--achievements text-lg md:text-2xl font-extrabold border-2 border-white-400 backdrop-filter backdrop-blur-sm bg-black bg-opacity-10 rounded-xl p-2 sm:p-4 mr-4 ml-4 transition hover:scale-105">
           <span className="hidden lg:inline">Achievements</span> 🏆
         </button>
       </Link>
