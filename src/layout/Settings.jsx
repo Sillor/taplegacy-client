8df424f693b1cdabbfa0319228dd8f5a307e851d
Egor <65542192+Sillor@users.noreply.github.com>
2023-12-26 17:45:31 -0800

    Improve settings page style

--- src/layout/Settings.jsx
+++ src/layout/Settings.jsx
@@ -41,7 +41,7 @@ function Settings({ userData, setUserData }) {
 
   return (
     <div className="page-container flex justify-center items-center">
-      <div className="md:max-w-[60vw] xl:max-w-3xl md:h-[80vh] overflow-y-auto p-4 md:px-6 h-full w-full bg-black rounded-md bg-clip-padding bg-opacity-60 border-2 border-white no-scrollbar flex flex-col items-start gap-4">
+      <div className="md:max-w-[60vw] xl:max-w-3xl overflow-y-auto p-4 md:px-6 h-full w-full bg-black rounded-md bg-clip-padding bg-opacity-60 border-2 border-white no-scrollbar flex flex-col items-start gap-4">
         <h1 className="font-bold text-3xl mt-2 ml-2">Settings</h1>
         <h1 className="font-bold text-2xl ml-6">Stats:</h1>
         <div className="data sm:text-xl ml-10">
@@ -66,26 +66,27 @@ function Settings({ userData, setUserData }) {
           </p>
         </div>
 
-        <div className="flex gap-4">
-          <button
-            className="text-md font-extrabold border-2 bg-black bg-opacity-30 rounded-md p-2 text-red-600 mt-8 self-end"
-            onClick={handleRemoveData}
-          >
-            Remove data
-          </button>
-          <button
-            className="text-md font-extrabold border-2 bg-black bg-opacity-30 rounded-md p-2 mt-8 self-end"
-            onClick={handleLogout}
-          >
-            Log Out
-          </button>
+        <div className="flex justify-between gap-4 w-full">
+          <div className="flex gap-4">
+            <button
+              className="text-md font-extrabold border-2 bg-black bg-opacity-30 rounded-md p-2 text-red-600 mt-8"
+              onClick={handleRemoveData}
+            >
+              Remove data
+            </button>
+            <button
+              className="text-md font-extrabold border-2 bg-black bg-opacity-30 rounded-md p-2 mt-8"
+              onClick={handleLogout}
+            >
+              Log Out
+            </button>
+          </div>
+          <Link to="/" className="hideoverlay self-end">
+            <button className="text-md font-extrabold border-2 bg-black bg-opacity-30 rounded-md p-2">
+              Back
+            </button>
+          </Link>
         </div>
-
-        <Link to="/" className="hideoverlay self-end">
-          <button className="text-md font-extrabold border-2 bg-black bg-opacity-30 rounded-md p-2">
-            Back
-          </button>
-        </Link>
       </div>
     </div>
   );
